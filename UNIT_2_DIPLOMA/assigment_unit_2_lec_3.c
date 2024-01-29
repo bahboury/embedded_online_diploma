fed22ef5e5e610d20ab2038ec41ec7da9146f47f
bahy mohy <bahymohy2004@gmail.com>
2024-01-29 23:00:19 +0200

    homework1

--- UNIT_2_DIPLOMA/assigment_unit_2_lec_3.c
+++ UNIT_2_DIPLOMA/assigment_unit_2_lec_3.c
@@ -39,7 +39,96 @@
 //     printf("sum = %d",sum);
 // }
 
-//########## Example 3 ##########
+//########## Example 4 ##########
 
-#include<stdio.h>
-#include<stdlib.h>
+// #include<stdio.h>
+// #include<stdlib.h>
+
+
+// int main()
+// {
+//     float num_1 , num_2 , product ;
+
+//     printf("enter 2 numbers : ");
+
+//     fflush(stdin);fflush(stdout);
+
+//     scanf("%f %f",&num_1,&num_2);
+
+//     product = num_1 * num_2 ;
+
+//     printf("product = %.2f",product);
+
+// }
+
+
+//########## Example 5 ##########
+
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+
+// int main()
+// {
+//     char _char ;
+
+//     printf("enter a character : ");
+
+//     fflush(stdin);fflush(stdout);
+
+//     scanf("%c",&_char);
+
+//     printf("ASCII value of G = %d",_char);
+// }
+
+//########## Example 6 ##########
+
+
+// #include<stdio.h>
+// #include<stdlib.h>
+
+
+// int main()
+// {
+//     double a , b , temp = 0;
+
+    // printf("Enter value of a = ");
+    // fflush(stdin);fflush(stdout);
+    // scanf("%lf",&a);
+
+
+    // printf("Enter value of b = ");
+    // fflush(stdin);fflush(stdout);
+    // scanf("%lf",&b);
+
+//     temp = a ; a = b ; b = temp ;
+
+//     printf("After swapping , value of a = %.2lf",a);
+//     printf("\nAfter swapping , value of b = %.2lf",b);
+
+// }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int x , y;
+
+    printf("Enter value of x = ");
+    fflush(stdin);fflush(stdout);
+    scanf("%d",&x);
+
+
+    printf("Enter value of y = ");
+    fflush(stdin);fflush(stdout);
+    scanf("%d",&y);
+    
+    x = x + y ;  y = x - y; x = x - y ;
+
+    printf("\nAfter swapping: x = %d , y = %d\n", x, y);
+
+    return 0;
+}
